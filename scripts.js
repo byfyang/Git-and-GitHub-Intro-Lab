650c74911d874c85c5ea16c762f4d2df1dd30908
mnorelli <mnorelli360@gmail.com>
2016-05-09 11:53:06 -0700

    yo! the button is working now

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // JavaScript to be added
-var emailAddress = prompt("please enter your email address");
+window.onload =function(){
+
+var changeButton = document.querySelector("#button-yellow");
+console.log("changeButton is " + changeButton);
+changeButton.addEventListener("click",function(){
+  var emailAddress = prompt("please enter your email address");
+  changeButton.textContent = "Thanks for your email!";
+});
+
+}
